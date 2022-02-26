9f400050cb705a4fd458cb86842fc3fc5a8333da
Liam Fotheringham <2595981F@student.gla.ac.uk>
2022-02-26 10:59:01 +0000

    Add project files.

---
+++ Weather Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Weather_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
