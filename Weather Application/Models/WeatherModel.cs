aa8e274047266a3e22dfad7806e87a3a9b80e229
Liam Fotheringham <2595981F@student.gla.ac.uk>
2022-02-26 21:23:35 +0000

    WeatherModel.cs - Update properties of classes to include underscores

--- Weather Application/Models/WeatherModel.cs
+++ Weather Application/Models/WeatherModel.cs
@@ -14,36 +14,36 @@
         public string Country { get; set; }
         public float Lat { get; set; }
         public float Lon { get; set; }
-        public string TzId { get; set; }
-        public int LocaltimeEpoch { get; set; }
+        public string Tz_id { get; set; }
+        public int Localtime_epoch { get; set; }
         public string Localtime { get; set; }
     }
 
     public class Current
     {
-        public int LastUpdatedEpoch { get; set; }
-        public string LastUpdated { get; set; }
-        public float TempC { get; set; }
-        public float TempF { get; set; }
-        public int IsDay { get; set; }
+        public int Last_updated_epoch { get; set; }
+        public string Last_updated { get; set; }
+        public float Temp_c { get; set; }
+        public float Temp_f { get; set; }
+        public int Is_day { get; set; }
         public Condition Condition { get; set; }
-        public float WindMph { get; set; }
-        public float WindKph { get; set; }
-        public int WindDegree { get; set; }
-        public string WindDir { get; set; }
-        public float PressureMb { get; set; }
-        public float PressureIn { get; set; }
-        public float PrecipMm { get; set; }
-        public float PrecipIn { get; set; }
+        public float Wind_mph { get; set; }
+        public float Wind_kph { get; set; }
+        public int Wind_degree { get; set; }
+        public string Wind_dir { get; set; }
+        public float Pressure_mb { get; set; }
+        public float Pressure_in { get; set; }
+        public float Precip_mm { get; set; }
+        public float Precip_in { get; set; }
         public int Humidity { get; set; }
         public int Cloud { get; set; }
-        public float FeelslikeC { get; set; }
-        public float GeelslikeF { get; set; }
-        public float VisKm { get; set; }
-        public float VisMiles { get; set; }
+        public float Feelslike_c { get; set; }
+        public float Feelslike_f { get; set; }
+        public float Vis_km { get; set; }
+        public float Vis_miles { get; set; }
         public float Uv { get; set; }
-        public float GustMph { get; set; }
-        public float GustKph { get; set; }
+        public float Gust_mph { get; set; }
+        public float Gust_kph { get; set; }
     }
 
     public class Condition
