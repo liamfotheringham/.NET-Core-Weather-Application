f35455985fa43ff7d764d1981d52e8c4c3aae26a
Liam Fotheringham <2595981F@student.gla.ac.uk>
2022-02-26 21:28:21 +0000

    Views, Controller and Service for Weather API interface in application

---
+++ Weather Application/Controllers/WeatherController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Weather_Application.Models;
+using Weather_Application.Services;
+
+namespace Weather_Application.Controllers
+{
+    [ApiController]
+    [Route("weather")]
+    [Authorize]
+    public class WeatherController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly WeatherLookupService _weatherLookupService;
+
+        public WeatherController(ILogger<HomeController> logger, WeatherLookupService weatherLookupService)
+        {
+            _logger = logger;
+            _weatherLookupService = weatherLookupService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("query")]
+        public async Task<IActionResult> GetWeatherData(string query)
+        {
+            if(query == null)
+            {
+                throw new NullReferenceException("Query is required");
+            }
+            try
+            {
+                var weatherData = await _weatherLookupService.GetWeatherData(query);
+                return View("ViewWeatherData", weatherData);
+            }
+            catch(Exception ex)
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ Weather Application/Services/WeatherLookupService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using Weather_Application.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Weather_Application.Services
+{
+    public class WeatherLookupService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IOptions<WeatherAPIModel> _options;
+
+        public WeatherLookupService(HttpClient httpClient, IOptions<WeatherAPIModel> options)
+        {
+            _httpClient = httpClient;
+            _options = options;
+        }
+
+        public async Task<WeatherModel> GetWeatherData(string query)
+        {
+            var url = BuildWeatherQuery(query);
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Request has failed");
+            }
+
+            var seralizedResponse = await response.Content.ReadAsStringAsync();
+            var weatherModelResponse = JsonConvert.DeserializeObject<WeatherModel>(seralizedResponse);
+
+            return weatherModelResponse;
+        }
+
+        private string BuildWeatherQuery(string query)
+        {
+            var url = _options.Value.BaseUrl;
+            var queryParameters = $"key={_options.Value.Key}&q={query}";
+
+            return $"{url}{queryParameters}";
+        }
+    }
+}
