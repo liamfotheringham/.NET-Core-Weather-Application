52f819cdf9d6d09afd654cc5d78db31036b3ae3f
Liam Fotheringham <2595981F@student.gla.ac.uk>
2022-02-27 17:42:44 +0000

    Addition of UnitTesting Project

---
+++ Weather Application Tests/WeatherControllerTest.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace Weather_Application_Tests
+{
+    public class WeatherControllerTest
+    {
+
+        public WeatherControllerTest()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
